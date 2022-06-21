9f837fc648d31474dcfdbd07da15d3eaf6fe874e
Igor Oliveira <igor.oliveira@valtech.com>
2022-06-21 17:47:09 -0300

    basic cypress last mesmo

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -248,7 +248,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
           })
       })
 
-      it.only('encontra o gato escondido', function() {
+      it('encontra o gato escondido', function() {
         cy.get('#cat')
           .invoke('show')
           .should('be.visible')
